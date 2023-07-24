7a4b6e0bd34f13191b691fb15f0895269482183a
Torben Hoffmann <lehoff@protonmail.com>
2023-07-24 22:54:11 +0200

    Initial commit

---
+++ lib/whoscored_spider.ex
@@ -0,0 +1,18 @@
+defmodule WhoscoredSpider do
+  @moduledoc """
+  Documentation for `WhoscoredSpider`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WhoscoredSpider.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
